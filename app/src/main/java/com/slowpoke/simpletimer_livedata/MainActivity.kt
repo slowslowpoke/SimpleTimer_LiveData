4b17d68eeaf88c9ccd0933d8782bcca641706855
slowpoke <amina.muradalieva91@gmail.com>
2023-12-06 17:00:04 +0600

    Added ViewModel and LiveData. Countdown works smoothly now.

--- app/src/main/java/com/slowpoke/simpletimer_livedata/MainActivity.kt
+++ app/src/main/java/com/slowpoke/simpletimer_livedata/MainActivity.kt
@@ -9,27 +9,23 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.slowpoke.simpletimer_livedata.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var timer: CountDownTimer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        timer = object: CountDownTimer(24000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                binding.tvNumber.text = (millisUntilFinished / 1000).toString()
-            }
+        val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewModel.startTimer()
 
-            override fun onFinish() {
-                Toast.makeText(this@MainActivity, "Countdown is done!", Toast.LENGTH_SHORT).show()
-            }
-
-        }.start()
+        viewModel.seconds.observe(this) {
+            binding.tvNumber.text = viewModel.seconds.value.toString()
+        }
     }
 
 }
--- app/src/main/java/com/slowpoke/simpletimer_livedata/MainActivityViewModel.kt
+++ app/src/main/java/com/slowpoke/simpletimer_livedata/MainActivityViewModel.kt
@@ -1,6 +1,31 @@
 package com.slowpoke.simpletimer_livedata
 
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel: ViewModel() {
+    private lateinit var timer: CountDownTimer
+    private val _seconds = MutableLiveData<Int>()
+    val seconds: LiveData<Int>
+        get() = _seconds
+
+    fun startTimer(){
+        timer = object: CountDownTimer(10000, 1000){
+
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1000
+                _seconds.value = timeLeft.toInt()
+            }
+
+            override fun onFinish() {
+            }
+
+        }.start()
+    }
+
+    fun stopTimer(){
+        timer.cancel()
+    }
 }
